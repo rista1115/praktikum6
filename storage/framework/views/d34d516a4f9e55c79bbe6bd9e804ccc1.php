f074a6a42fca7eed675da9698302914b37b1228c
rista1115 <163830773+rista1115@users.noreply.github.com>
2024-04-07 21:32:43 +0700

    Add files via upload

---
+++ storage/framework/views/d34d516a4f9e55c79bbe6bd9e804ccc1.php
@@ -0,0 +1,8 @@
+<?php $__env->startSection('content'); ?>
+    <?php echo $__env->make('layouts.default', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?>
+<?php $__env->stopSection(); ?>
+
+
+
+
+<?php echo $__env->make('layouts.app', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH C:\Users\maris\rista-app\modul4\resources\views/home.blade.php ENDPATH**/ ?>
